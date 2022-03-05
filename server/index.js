e3819c5b9c3595d1655a7b0baec55efaca10d1df
AlexDoss42 <alex.doss42@gmail.com>
2022-03-05 14:35:10 -0700

    adds add employee route

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+const pool = require('./db');
+
+app.use(express.json());
+
+app.post("/employee", async (req, res) => {
+    try {
+        const { name, date_of_joining, designation, gender, email, bio } = req.body;
+        const employeeData = await pool.query("INSERT INTO EMPLOYEE(name, date_of_joining, designation, gender, email, bio) VALUE($1, $2, $3, $4, $5, $6)", [name, date_of_joining, designation, gender, email, bio]);
+        res.json(req.body);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 
 app.listen(3000, () => {
